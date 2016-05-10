9dd9a87249620f3f1a20062082410a75e018eb93
Takashi Kitajima <inc@2inc.org>
2016-05-10 21:51:34 +0900

    Change to use sass-basis components

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
-import BasisDrawer from './drawer.js';
+import BasisDrawer from '../../node_modules/sass-basis-drawer/src/js/drawer.js';
+import BasisMenu from '../../node_modules/sass-basis-menu/src/js/menu.js';
 
 new BasisDrawer();
+new BasisMenu();
